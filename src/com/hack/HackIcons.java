78cae86f00544b0e9e6d01c2ac059301e9c9e911
Kevin Jackson <kevin@checkfront.com>
2016-10-10 21:02:42 -0700

    Replaced file icon with standard hack icon

--- src/com/hack/HackIcons.java
+++ src/com/hack/HackIcons.java
@@ -4,5 +4,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class HackIcons {
-    public static final Icon FILE = IconLoader.getIcon("/com/hack/icons/h.png");
+    public static final Icon FILE = IconLoader.getIcon("/com/hack/icons/hh.png");
 }
